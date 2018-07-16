47e85c4ddbabdf8aef2699e7ac55ff9faec8aa07
lee <lee>
2018-07-16 12:06:49 +0800

    all repo

--- sync.js
+++ sync.js
@@ -14,6 +14,51 @@ function transRepoName(repoName){
 	return repoName.replace('gcr.io',registryName);
 }
 
+function getFirstVer(ver){
+	if(!/^\d+(\.\d+)*(-\d+)*$/.test(ver)){
+		return -1;
+	}
+
+	var parts = ver.split('-');
+	parts = parts[0].split('.');
+	return parseInt(parts[0]);
+}
+
+
+function filterTagList(existTagList,allTagList){
+	var shouldIgnoreOldVer = allTagList.length >= 10;
+	var largestVer = 0;
+	for(var idx=0;idx<allTagList.length;idx++){
+		var firstVer = getFirstVer(allTagList[idx]);
+		if(firstVer>largestVer){
+			largestVer = firstVer;
+		}
+	};
+	console.log('largestVer:'+largestVer);
+
+
+	let targetTagList = [];
+	let tagList = allTagList;
+	for(var idx=0;idx<tagList.length;idx++){
+		if(existTagList.indexOf(tagList[idx])<0 || tagList[idx] === 'latest'){
+
+			if(shouldIgnoreOldVer && tagList[idx] !== 'latest'){
+				var firstVer = getFirstVer(tagList[idx]);
+				if(firstVer >= 0  && largestVer - firstVer >=2){
+					
+				}else{
+					targetTagList.push(tagList[idx]);
+				}
+
+			}else{
+				targetTagList.push(tagList[idx]);	
+			}
+		}
+	}
+	return targetTagList;
+
+}
+
 function syncImages(repoName,tagList){
 	for(var idx=0;idx<tagList.length;idx++){
 		shell.exec('docker pull '+repoName+':'+tagList[idx]);
@@ -76,16 +121,11 @@ function handleOneRepo(repoName){
 	let existTagList = [];
 
 	return listExistRepoTags(repoName).then(function(tagList){
+		console.log('tagList:'+JSON.stringify(tagList));
 		existTagList = tagList;
 		return listRepoTags(repoName);
 	}).then(function (tagList){
-		let targetTagList = [];
-		for(let idx=0;idx<tagList.length;idx++){
-			if(existTagList.indexOf(tagList[idx])<0 || tagList[idx] === 'latest'){
-				targetTagList.push(tagList[idx]);
-			}
-		}
-
+		let targetTagList = filterTagList(existTagList,tagList);
 		syncImages(repoName,targetTagList);
 
 	}).then(function(){
@@ -97,13 +137,39 @@ function handleOneRepo(repoName){
 }
 
 
-var REPO = 'gcr.io/spinnaker-marketplace/clouddriver';
+function handleRepoList(list){
+	if(list.length === 0){
+		return Promise.resolve();		
+	}else{
+		let oneRepo = list[0];
+		list.splice(0,1);
+		return handleOneRepo(oneRepo).then(function(){
+			return handleRepoList(list);
+		});
+	}
+}
+
+var fs=require('fs');
+var filePath="./repo.data";
+var repoContent=fs.readFileSync(filePath).toString();
+var repoLines = repoContent.split('\n');
+var repoList = [];
+repoLines.forEach(aLine=>{
+	if(aLine.length>0){
+		repoList.push(aLine);
+	}
+});
+handleRepoList(repoList);
+
+
+//var REPO = 'gcr.io/spinnaker-marketplace/clouddriver';
 //REPO = 'jwilder/nginx-proxy';
 // var REPO = 'registry.sxlbzm.com/nodeapp'
 //47.52.238.92 
 //var client = drc.createClientV1({name: REPO});
 
-handleOneRepo(REPO);
+// handleOneRepo(REPO);
+
 
 
 
